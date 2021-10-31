ecc3660802d1c3fc97a462aad520feded0ebe409
Rahul Noronha <rahulnor@gmail.com>
2021-10-31 07:53:12 +0530

    Added react app

---
+++ form/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Resume from './screens/Resume';
+import ResumeScreen from './screens/ResumeScreen';
+import Header from './screens/Header';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
+ 
+function App() {
+  return (
+    <Router>
+
+   
+    <div className="app">
+        <Switch>
+          
+          <Route path="/resume">
+            <Header/>
+            <ResumeScreen />
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Resume />
+          </Route>
+
+         
+      </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
